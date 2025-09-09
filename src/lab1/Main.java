a53ce9f48dcd860c8bfdb33bcde569faa77db9db
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-09-10 03:42:24 +1100

    multiples file and function created

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.main(null)
+        FizzBuzz.main(null);
         FizzBuzz.doFizzBuzz(5);
     }
 }
